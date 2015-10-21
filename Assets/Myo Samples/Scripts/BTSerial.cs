71db111fa43e4a9107c0e1c75f96ca654f0c883f
timowang1991 <b99611045@ntu.edu.tw>
2015-10-21 22:36:11 +0800

    Myo connection, Unity3d underwater scene, Unity3d shark model ready

---
+++ Assets/Myo Samples/Scripts/BTSerial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class BTSerial : MonoBehaviour {
+	SerialPort serial;
+	public string[] portNames;
+
+	// Use this for initialization
+	void Start () {
+		portNames = SerialPort.GetPortNames();
+		Debug.Log("portNames : " + portNames);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnValidate(){
+		Debug.Log("OnValidate");
+		serial = new SerialPort("/dev/tty.HC-06-DevB", 115200);
+	}
+}
