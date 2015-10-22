3120332811740499cd9d88350529ca9b6eb064c0
timowang1991 <b99611045@ntu.edu.tw>
2015-10-22 15:50:03 +0800

    complete gui design for port selection

--- Assets/Myo Samples/Scripts/BTSerial.cs
+++ Assets/Myo Samples/Scripts/BTSerial.cs
@@ -1,24 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO.Ports;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.UI.Extensions;
+using System;
 
 public class BTSerial : MonoBehaviour {
-	SerialPort serial;
-	public string[] portNames;
+	SerialPort serial = new SerialPort("/dev/tty.KPBluetooth20-DevB", 38400);
+
+	public GameObject dropDownListGO;
+	DropDownList dropDownList;
 
 	// Use this for initialization
 	void Start () {
-		portNames = SerialPort.GetPortNames();
-		Debug.Log("portNames : " + portNames);
+		dropDownList = dropDownListGO.GetComponent<DropDownList>();
+		dropDownList.OnSelectionChanged += OnPortSelect;
+		populateDropDownList();
+
+
+//		try{
+//			serial.Open();
+//		}catch (UnityException unityException){
+//			Debug.Log("UnityException : " + unityException.Message);
+//		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+//		if (Input.anyKey){
+//			Debug.Log("anyKey is pressed");
+//			serial.Write("a");
+//		}
 	}
 
-	void OnValidate(){
-		Debug.Log("OnValidate");
-		serial = new SerialPort("/dev/tty.HC-06-DevB", 115200);
+	void populateDropDownList(){
+		List<string> portNames = GetPortNames();
+		dropDownList.Items = new List<DropDownListItem>();
+
+		foreach(string portName in portNames){
+			DropDownListItem item = new DropDownListItem(portName);
+			dropDownList.Items.Add(item);
+		}
+
+//		dropDownList.ItemsToDisplay = dropDownList.Items.Count;
+	}
+
+	List<string> GetPortNames()
+	{
+		int p = (int)System.Environment.OSVersion.Platform;
+		List<string> serial_ports = new List<string> ();
+		
+		// Are we on Unix?
+		if (p == 4 || p == 128 || p == 6) {
+			string[] ttys = Directory.GetFiles ("/dev/", "tty.*");
+			string[] cuFiles = Directory.GetFiles("/dev/", "cu.*");
+			foreach (string dev in ttys) {
+				if (dev.StartsWith ("/dev/tty.")){
+					serial_ports.Add (dev);
+					Debug.Log (dev);
+				}
+			}
+			foreach (string dev in cuFiles) {
+				if (dev.StartsWith ("/dev/cu.")){
+					serial_ports.Add (dev);
+					Debug.Log (dev);
+				}
+			}
+		}
+
+		return serial_ports;
+	}
+
+	void OnPortSelect(int idx){
+		Debug.Log("Port selected : " + dropDownList.Items[idx].Caption);
 	}
 }
